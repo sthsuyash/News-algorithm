2363064f79c2876c5b447411653ec75064834141
Suyash Shrestha <sthasuyash11@gmail.com>
2025-01-27 10:04:45 +0545

    [FIX]-update on recommendation route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,13 @@
 from .summarizer_route import router as summarizer
 from .translator_route import router as translator
 from .classifier_route import router as classifier
-from .recommendor_route import router as recommendor
+from .recommendor_route import router as news_recommender
 from .sentiment_analyer_route import router as sentiment_analyer
+
+_all__ = [
+    summarizer,
+    translator,
+    classifier,
+    news_recommender,
+    sentiment_analyer,
+]
--- app/routes/recommendor_route.py
+++ app/routes/recommendor_route.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+import os
 from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException
 
 from app.core.response_model import ResponseModel
 from app.core.logging import setup_logging
@@ -7,7 +8,26 @@ from app.algorithms.news_recommender import NewsRecommendationSystem
 
 router = APIRouter()
 logger = setup_logging()
-recommender = NewsRecommendationSystem()
+
+# Define the path to the recommendation model
+MODEL_PATH = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), "../ml_models/news_recommendation_model.pkl"
+    )
+)
+
+# Check if the model file exists
+if not os.path.exists(MODEL_PATH):
+    raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
+
+# Load the recommendation model
+recommender = None
+try:
+    recommender = NewsRecommendationSystem.load_model(MODEL_PATH)
+    logger.info("Recommendation model loaded successfully.")
+except Exception as e:
+    logger.error(f"An error occurred while loading the recommendation model: {e}")
+    raise HTTPException(status_code=500, detail="Failed to load recommendation model.")
 
 
 class NewsRecommendationRequest(BaseModel):
@@ -28,12 +48,19 @@ async def recommend_news(request: NewsRecommendationRequest):
     try:
         article_id = request.article_id
 
-        recommended_articles = recommender.recommend_news(article_id)
+        # Get recommendations from the model
+        recommendations = recommender.recommend(article_id, limit=5)
+
+        # Format the recommendations
+        recommended_articles = [
+            {"article_id": rec_id, "score": float(score)}
+            for rec_id, score in recommendations
+        ]
 
         return ResponseModel(
             status_code=200,
             message="News recommendation successful.",
-            data={"recommended_articles": recommended_articles},
+            data={"recommendations": recommended_articles},
             error=None,
         )
 
