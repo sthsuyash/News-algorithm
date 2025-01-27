9d5c5ac9aae99a283811696c06999f3fd017383e
Suyash Shrestha <sthasuyash11@gmail.com>
2025-01-27 10:05:17 +0545

    [FIX]-configuration of ai server update

--- app/core/config.py
+++ app/core/config.py
@@ -1,20 +1,7 @@
 import os
-from typing import Annotated, Any, Literal
+from typing import Literal
 
-from pydantic import (
-    AnyUrl,
-    BeforeValidator,
-    computed_field,
-)
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-def parse_cors(v: Any) -> list[str] | str:
-    if isinstance(v, str) and not v.startswith("["):
-        return [i.strip() for i in v.split(",")]
-    elif isinstance(v, list | str):
-        return v
-    raise ValueError(v)
 
 
 class Settings(BaseSettings):
@@ -26,23 +13,9 @@ class Settings(BaseSettings):
         env_ignore_empty=True,
         extra="ignore",
     )
+
     API_V1_STR: str = "/api/v1"
-    # 60 minutes * 24 hours * 8 days = 8 days
-    FRONTEND_HOST: str = "http://localhost:5005"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
-    BACKEND_CORS_ORIGINS: Annotated[
-        list[AnyUrl] | str, 
-        BeforeValidator(parse_cors)
-    ] = []
 
-    @computed_field 
-    @property
-    def all_cors_origins(self) -> list[str]:
-        return [
-            str(origin).rstrip("/") 
-            for origin in self.BACKEND_CORS_ORIGINS
-        ] + [self.FRONTEND_HOST]
-
-
-settings = Settings() 
+settings = Settings()
